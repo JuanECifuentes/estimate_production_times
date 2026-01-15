c72e8c902160ed7d3330c488952126d38eed3780
JuanECifuentes <juancifuentes124@gmail.com>
2026-01-15 18:00:50 -0500

    ajuste env

--- production_estimator/settings.py
+++ production_estimator/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jjv^6*wsj@=lqnsi3i5yosb875mc3!8v!ska3-@vga+3uozm!#'
+SECRET_KEY = os.getenv("SECRET_KEY", "S#perS3crEt_1122")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
